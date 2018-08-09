06189fea1a4c5797f3f161cce7318a0524658067
pridiltal <priyangt@monash.edu>
2018-08-09 15:04:50 +1000

    added travis badge

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Water Quality Sensor data - Sandy_Creek
+#'
+#' A multivariate dataset containing the variables obtained using water quality
+#' sensors from Sandy Creek .
+#'
+#' @format A data frame with 15231 rows and 15 variables:
+#' \describe{
+#' \item{Timestamp }{Time Stamps}
+#' \item{Lmain_Level_m }{Lmain_Level_m }
+#' \item{Lmain_BattVMin}{Lmain_BattVMin}
+#' \item{Lmain_Temp_C}{Lmain_Temp_C}
+#' \item{Lmain_PestFrVMin}{Lmain_PestFrVMin}
+#' \item{Lmain_PumpBattVMin}{Lmain_PumpBattVMin}
+#' \item{Lsonde_Cond_uscm}{Lsonde_Cond_uscm}\
+#' \item{Lsonde_DO_mgL}{Lsonde_DO_mgL}
+#' \item{Lsonde_DO_pSat}{Lsonde_DO_pSat}
+#' \item{Lsonde_fDOM_QSU}{Lsonde_fDOM_QSU}
+#' \item{Lsonde_fDOM_Raw}{Lsonde_fDOM_Raw}
+#' \item{Lsonde_fDOM_RFU}{Lsonde_fDOM_RFU}
+#' \item{Lsonde_Level_m}{Lsonde_Level_m}
+#' \item{Lsonde_Temp_C}{Lsonde_Temp_C}
+#' \item{Lsonde_Turb_NTU}{Lsonde_Turb_NTU}
+#' }
+"sandy_creek"
---
+++ R/oddwater.R
@@ -0,0 +1,8 @@
+#' oddwater: A package for Outlier Detection in water quality sensor data
+
+#' @author Priyanga Dilini Talagala, Rob J. Hyndman
+#' @seealso The core functions in this package: \code{\link{transform_data}},
+#' \code{\link{plot_series}}, \code{\link{plot_pairs}}
+#' @docType package
+#' @name  oddstream
+NULL
