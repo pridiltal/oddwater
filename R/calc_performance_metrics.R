b27cea01046593b8bc6c2ce0366e8986a3fd284b
Priyanga Talagala <pritalagala@gmail.com>
2018-09-11 20:05:00 +1000

    added PPV and NPV to cal_performance-metric function

--- R/calc_performance_metrics.R
+++ R/calc_performance_metrics.R
@@ -19,6 +19,8 @@
 #' \item{Recall}{Recall}
 #' \item{F_Measure}{F Measure}
 #' \item{Optimised_Precision}{Optimised Precision}
+#' \item{PPV}{Positive Predictive Value}
+#' \item{NPV}{Negative Predictive Value}
 #' @export
 #' @author Priyanga Dilini Talagala
 #' @examples
@@ -29,43 +31,50 @@
 #' out<- calc_performance_metrics(y_truth = true_labels, y_output = output,
 #'                               pos_label = "out", neg_label = "normal")
 #'
-calc_performance_metrics <- function(y_truth, y_output,  pos_label, neg_label, print_out = TRUE)
-{
+calc_performance_metrics <- function(y_truth, y_output, pos_label, neg_label, print_out = TRUE) {
   # Recoding Data
-  oldvalues <- c(pos_label, neg_label )
+  oldvalues <- c(pos_label, neg_label)
   newvalues <- c(1, 0)
   y_truth <- newvalues[ match(y_truth, oldvalues)]
   y_output <- newvalues[ match(y_output, oldvalues)]
-  data <- data.frame(y_truth, y_output )
+  data <- data.frame(y_truth, y_output)
 
   # Get confusion matrix
-  TN   <- sum(data$y_truth == 0 & data$y_output == 0)
-  FN   <- sum(data$y_truth == 1 & data$y_output == 0)
-  FP   <- sum(data$y_truth == 0 & data$y_output == 1)
-  TP   <- sum(data$y_truth == 1 & data$y_output == 1)
-  acc  <- (TP + TN) / (TP + FP + TN + FN)
-  err  <- (FP + FN) / (TP + FP + TN + FN)
-  sn   <- TP / (TP + FN)
-  sp   <- TN / (TN + FP)
-  p    <- TP / (TP + FP)
-  r    <- TP / (TP + TN)
-  FM   <- (2*p*r) / (p + r)
-  GM   <- sqrt(TP*TN)
+  TN <- sum(data$y_truth == 0 & data$y_output == 0)
+  FN <- sum(data$y_truth == 1 & data$y_output == 0)
+  FP <- sum(data$y_truth == 0 & data$y_output == 1)
+  TP <- sum(data$y_truth == 1 & data$y_output == 1)
+  acc <- (TP + TN) / (TP + FP + TN + FN)
+  err <- (FP + FN) / (TP + FP + TN + FN)
+  sn <- TP / (TP + FN)
+  sp <- TN / (TN + FP)
+  p <- TP / (TP + FP)
+  r <- TP / (TP + TN)
+  FM <- (2 * p * r) / (p + r)
+  GM <- sqrt(TP * TN)
 
-  Nn   <- (FP + TN) / (TP + FP + TN + FN)
-  Np   <- (TP + FN) / (TP + FP + TN + FN)
-  P    <- (sp*Nn + sn*Np)
-  RI   <- (abs(sp - sn)/(sp+sn))
-  OP   <- P - RI
-  pm <- list(TN, FN, FP, TP, acc, err, sn, sp, p, r, FM, GM, OP)
-  names(pm) <-c("TN", "FN", "FP", "TP", "Accuracy", "Error_Rate", "Sensitivity",
-                "Specificity", "Precision", "Recall", "F_Measure","Geometric_mean", "Optimised_Precision")
+  Nn <- (FP + TN) / (TP + FP + TN + FN)
+  Np <- (TP + FN) / (TP + FP + TN + FN)
+  P <- (sp * Nn + sn * Np)
+  RI <- (abs(sp - sn) / (sp + sn))
+  OP <- P - RI
 
-  if(print_out)
-  {print(data.frame(Value = round(unlist(pm),4)))}
+  PPV <- TP / (TP + FP)
+  NPV <- TN / (TN + FN)
+
+  pm <- list(TN, FN, FP, TP, acc, err, sn, sp, p, r, FM, GM, OP, PPV, NPV)
+  names(pm) <- c(
+    "TN", "FN", "FP", "TP", "Accuracy", "Error_Rate", "Sensitivity",
+    "Specificity", "Precision", "Recall", "F_Measure", "Geometric_mean",
+    "Optimised_Precision", "Positive_Predictive_Value",
+    "Negative_Predictive_Value"
+  )
+
+  if (print_out) {
+    print(data.frame(Value = round(unlist(pm), 4)))
+  }
 
   return(pm)
-
 }
 
 #' Compute mean squared error
@@ -76,9 +85,7 @@ calc_performance_metrics <- function(y_truth, y_output,  pos_label, neg_label, p
 #' @export
 #' @author Priyanga Dilini Talagala
 #'
-calc_MSE <- function(y_truth, y_pred )
-{
+calc_MSE <- function(y_truth, y_pred) {
   MSE <- mean((y_truth - y_pred)^2)
   return(MSE)
 }
-
