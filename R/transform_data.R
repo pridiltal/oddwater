cc94b9d1f1cf1024de7ff2b6f6f155963259c42b
Priyanga Talagala <pritalagala@gmail.com>
2018-11-06 18:51:42 +1100

    added retlative difference

--- R/transform_data.R
+++ R/transform_data.R
@@ -67,6 +67,12 @@ transform_data <- function(data,  time_bound = 90, regular = FALSE, time_col = "
   colnames(ratio_series) <- paste("ratio_", colnames(data_var), sep = "")
   data <- cbind(data, ratio_series)
 
+  # Relative difference
+  relative_series <- rbind( rep(NA, ncol(data_var)), (data_var[2:(n-1),] - (1/2)*(data_var[1:(n-2),] +data_var[3:n,]  )),
+                            rep(NA, ncol(data_var)))
+  colnames(relative_series) <- paste("rdiff_", colnames(data_var), sep = "")
+  data <- cbind(data, relative_series)
+
   data <- tsibble::as_tsibble(data, index = time_col , regular = regular)
 
   return(data)
